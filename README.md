482c0b99105f314430055f74a94e4492351a1560
huiju04 <henjou04@naver.com>
2023-11-15 18:09:04 +0900

    씨발 내가 이걸 왜 하냐

--- README.md
+++ README.md
@@ -3,18 +3,19 @@
 ## 설치 항목
 
 - [x] styled-components : npm install styled-components
+- [x] Get Started : npm install react-hook-form
 - [x] react-router-dom : npm install react-router-dom
 - [x] 스타일 리셋
 - [] 폼 관련
 - [] 타이틀관련
 - [] 폰트어썸 관련
-- [] Get Started : npm install react-hook-form
+- [] swiper : npm install swiper
 
 ## todo
 
 - [x] router 설정
 - [x] 각 컴포넌트 제작
-- [] api 설정
+- [x] api 설정
 - [] Home ui 및 전체 구성 구성
 - [] Detail ui 및 전체 구성 컴포넌트 구성
 - [] Search ui 및 전체 구성 컴포넌트 구성
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,101 +1,45 @@
 import styled from "styled-components";
 import { nowPlaying } from "../../api";
 import { useEffect, useState } from "react";
-import { IMG_URL } from "../../constants";
-
-const MainBanner = styled.section`
-  height: 80vh;
-  background-color: lightgray;
-  position: relative;
-  padding: 400px 5%;
-  background: url(${IMG_URL}/original/${(props) => props.$bgUrl}) no-repeat
-    center / cover;
-  h3,
-  p {
-    position: relative;
-  }
-
-  h3 {
-    max-width: 650px;
-    width: 100%;
-    font-size: 80px;
-    font-weight: 700;
-    margin-bottom: 30px;
-    letter-spacing: -3px;
-    line-height: 100px;
-  }
-
-  p {
-    max-width: 650px;
-    width: 100%;
-    font-size: 18px;
-    font-weight: 400;
-    line-height: 26px;
-    opacity: 0.8;
-  }
-
-  @media screen and (max-width: 450px) {
-    h3 {
-      font-size: 50px;
-      line-height: 60px;
-    }
-
-    p {
-      font-size: 16px;
-    }
-  }
-`;
-
-const BlackBg = styled.div`
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(
-    0deg,
-    rgba(0, 0, 0, 1) 0%,
-    rgba(0, 0, 0, 0.8) 55%,
-    rgba(0, 0, 0, 0) 95%
-  );
-
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
+import { Banner } from "./Banner";
+import "swiper/css";
+import { ShowMovie } from "./ShowMovie";
+import { PacmanLoader } from "react-spinners";
 
 export const Home = () => {
-  // 1.마운트시 api에 요청
+  //1. 마운트시 api에 요청
   // 2.비동기 통신
   // 3.예외 처리
 
   const [nowPlayingData, setNowPlayingData] = useState();
-  const [isLoading, setisLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       try {
         const { results } = await nowPlaying();
         setNowPlayingData(results);
-        setisLoading(false);
+        setIsLoading(false);
       } catch (error) {
         console.log("에러:" + error);
       }
     })();
   }, []);
 
-  console.log(isLoading);
   console.log(nowPlayingData);
 
   return (
     <>
       {isLoading ? (
-        "loading..."
+        <PacmanLoader color="orange" />
       ) : (
         <div>
           {nowPlayingData && (
-            <MainBanner $bgUrl={nowPlayingData[0].backdrop_path}>
-              <BlackBg />
-              <h3>{nowPlayingData[0].title}</h3>
-              <p>{nowPlayingData[0].overview.slice(0, 100) + "..."}</p>
-            </MainBanner>
+            <>
+              <Banner data={nowPlayingData[0]} />
+
+              <ShowMovie movieData={nowPlayingData} />
+            </>
           )}
         </div>
       )}
